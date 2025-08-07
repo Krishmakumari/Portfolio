e955e6b3be8846e1b247e4defbb23b67cb673198
Krishma Kumari <krishma24604@gmail.com>
2025-08-08 00:27:55 +0530

    Sync local code with GitHub

---
+++ src/components/ReactLogo.jsx
@@ -0,0 +1,22 @@
+import React,{useRef} from 'react'
+import { Float ,useGLTF } from '@react-three/drei'
+const ReactLogo = (props) => {
+    const {nodes,materials}=useGLTF('/models/react.glb')
+  return (
+    <Float floatIntensity={1}>
+        <group position={[8,8,0]} scale={0.35} {...props} dispose={null}>
+            <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes['React-Logo_Material002_0'].geometry}
+            material={materials['Material.002']}
+            position={[0.5,0.5,0.5]}
+            rotation={[0,0,-Math.PI/2]}
+            scale={[0.5,0.5,0.5]}
+            />
+        </group>
+        </Float>
+  )
+}
+
+export default ReactLogo
